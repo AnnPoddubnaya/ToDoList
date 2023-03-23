d9bdc4089f12b3494fbd0c774690f93c406a77e0
Анна Поддубная <pod.dubnaya.anna1990@gmail>
2023-03-23 09:10:03 +0300

    add:LS

--- js/index.js
+++ js/index.js
@@ -1,30 +1,69 @@
+
 const inputToDo = document.querySelector('.todo__input');
 const buttonToDo = document.querySelector('.todo__button');
-const outToDo = document.querySelector('.todo__out');
-let toDoList = [];
+const todoList = document.querySelector('.todo__container');
+const containerToDo = document.querySelector('.todo');
 
-if (localStorage.getItem('todo')) {
-    toDoList = JSON.parse(localStorage.getItem('todo'));
-    addToDo();
+
+if (localStorage.getItem('taskToDo')) {
+    todoList.innerHTML = localStorage.getItem('taskToDo');
 }
-buttonToDo.addEventListener('click', () => {
-    let newToDo = {
-        text: inputToDo.value,
-        chekced: false,
+
+buttonToDo.addEventListener('click', createTodo);
+containerToDo.addEventListener('click', deleteToDo);
+containerToDo.addEventListener('click', doneToDo);
+
+function createTodo(event) {
+    event.preventDefault();
+    let todo = document.createElement('div');
+    todo.classList.add('todo__item');
+
+    let li = document.createElement("div");
+    li.innerText = `${inputToDo.value}`;
+    li.classList.add('todo__text');
+    todo.appendChild(li);
+
+    let buttonDone = document.createElement('button');
+    buttonDone.innerHTML = '<i class="fa-solid fa-check"></i>';
+    buttonDone.classList.add('todo__done');
+    todo.appendChild(buttonDone);
+
+    let buttonDelete = document.createElement('button');
+    buttonDelete.innerHTML = '<i class="fa-solid fa-circle-minus"></i>';
+    buttonDelete.classList.add('todo__delete');
+    todo.appendChild(buttonDelete);
+
+    if (inputToDo.value = '') {
+        alert('Please enter some text')
+    } else {
+        todoList.appendChild(todo);
     }
-    toDoList.push(newToDo);
-    // console.log(toDoList);
-    addToDo();
-    localStorage.setItem('todo', JSON.stringify(toDoList));
-});
 
-function addToDo() {
-    let toDo = '';
-    toDoList.forEach(item => {
-        toDo += `
-       <li class="todo__item">${item.text}</li>
-       `
-    })
-    outToDo.innerHTML = toDo;
-
+    inputToDo.value = '';
+    inputToDo.focus();
+    saveLocalStorage();
 }
+function deleteToDo(event) {
+    if (event.target.classList.contains('todo__delete')) {
+        const targetEl = event.target.closest('.todo__item')
+        targetEl.remove();
+    }
+    saveLocalStorage();
+}
+function doneToDo(event) {
+    if (event.target.classList.contains('todo__done')) {
+        const targetEl = event.target.closest('.todo__item')
+        targetEl.classList.toggle('done');
+    }
+    saveLocalStorage();
+}
+function saveLocalStorage() {
+    localStorage.setItem('taskToDo', todoList.innerHTML);
+}
+function editToDo(event) {
+    if (event.target.classList.contains('todo__text')) {
+        console.log('it is work')
+    }
+    // console.log(event)
+}
+editToDo()
